b8985b1c24c2be152b4c96c6a0f0ef144ec9b23e
최준영 <choi-junyoung@STELLA-JV-NT-2023-02.local>
2023-03-31 20:47:00 +0900

    chore: 프로젝트 생성

---
+++ src/main/kotlin/com/junyoung/withus/WithusApplication.kt
@@ -0,0 +1,11 @@
+package com.junyoung.withus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WithusApplication
+
+fun main(args: Array<String>) {
+    runApplication<WithusApplication>(*args)
+}
---
+++ src/test/kotlin/com/junyoung/withus/WithusApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.junyoung.withus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WithusApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
